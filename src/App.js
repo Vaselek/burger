0a8f38df1fcf632b9888e32345b7a803052ee66e
Vaselek <burzhuas@gmail.com>
2018-12-27 12:27:01 +0600

    Add state and main functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Burger from './Components/Burger/Burger';
+import ToppingInfo from './Components/ToppingInfo/ToppingInfo';
+
 
 class App extends Component {
+  state = {
+    toppings: {
+      cheese: {qty: 0, price: 20},
+      salad: {qty: 0, price: 5},
+      meat: {qty: 0, price: 50},
+      bacon: {qty: 0, price: 30}
+    },
+  };
+
+  addTopping = (topping) => {
+      let toppings = {...this.state.toppings};
+      toppings[topping].qty++;
+      this.setState({toppings});
+  };
+
+  removeTopping = (topping) => {
+      let toppings = {...this.state.toppings};
+      if (toppings[topping].qty !== 0) toppings[topping].qty--;
+      this.setState({toppings});
+  };
+
+  calcutalePrice = () => {
+      let price = 20;
+      for(let topping in this.state.toppings) {
+          price += this.state.toppings[topping].qty*this.state.toppings[topping].price;
+      }
+      return price;
+  };
+
   render() {
+    const toppings = Object.keys(this.state.toppings).map((topping, index) => {
+        return <ToppingInfo topping={topping}
+                            qty={this.state.toppings[topping].qty}
+                            index={index}
+                            onAdd={()=>this.addTopping(topping)}
+                            onRemove={()=>this.removeTopping(topping)}
+        />
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div className="container-fluid">
+              <div className="row">
+                  <div className="col-sm-6">
+                      <h3>Ingredients</h3>
+                      {toppings}
+                  </div>
+                  <div className="col-sm-6">
+                      <Burger toppings={this.state.toppings}/>
+                      <div style={{'text-align': 'center'}}>Price: {this.calcutalePrice()}</div>
+                  </div>
+              </div>
+          </div>
       </div>
     );
   }
